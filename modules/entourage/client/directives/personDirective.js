9ef410b2f6cf1588c5cf0434f85193f3e650bfa0
Cory Parrish <corybillparrish@gmail.com>
2014-03-10 08:06:06 -0400

    Beginning steps for entourage.

---
+++ modules/entourage/client/directives/personDirective.js
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cory
+ * Date: 3/8/14
+ * Time: 10:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+'use strict';
+
+module.exports = function () {
+    return {
+        restrict : 'E',
+        replace : true,
+        link: require('../links/personLink'),
+        templateUrl: '/component/entourage/person'
+    };
+};
---
+++ modules/entourage/client/links/personLink.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cory
+ * Date: 3/8/14
+ * Time: 10:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
