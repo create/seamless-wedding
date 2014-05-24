e02e1b727dd26e9631e78496a600f4ecfa779ea0
unknown <pyke.jonathan@gmail.com>
2014-05-24 16:50:00 -0700

    Jonathans updates

--- modules/story/client/services/storyService.js
+++ modules/story/client/services/storyService.js
@@ -58,24 +58,24 @@ module.exports = function () {
         },
         text: function () {
             return {
-                pre: "It was a cold winter’s night. Justine and Cory were at a bar called Fontana’s in the Lower (lower) East Side of Manhattan, borderline Chinatown. Locking eyes from across the bar, Justine caught the faint glimmer of Cory’s ginger locks in the dim light and Cory wondered what type of Asian Justine was. He proposed at that very moment and the rest is history. Just kidding, the story is actually longer than that.",
-                one: "Cory and Justine met at a mutual friend's birthday party at Fontana's, a local bar near Justine's apartment in Chinatown.  Post party, the night led the group of friends to Meatball Shop nearby. Cory took his seat beside Justine and proceeded to doodle on her hand with dry erase marker - a message that took up her entire arm....\"If (cute), call me\". Justine did not quite understand what this meant...\"If you're cute, call me?\" ...\"If I'm cute, call me?\" She would later learn that it was dorky computer code for \"If you think I'm cute, call me\". She would also later describe this event to friends as \"the night Cory javascripted me\".",
-                two: "Days later, Cory friended Justine on Facebook, only to learn that she was already spoken for by another man. It was not yet their time, but months would go by and they would cross paths again at a St. Patty's Day party at Cory's apartment in Hoboken (Justine was high school friends with his roommate). Justine would find out at this party that Cory was harboring a mini polar bear named Akimbo in his bedroom. She was immediately smitten by this giant fluffy creature and became more drawn to this man with his big white dog.",
-                three: "An essential thing to know about New Yorkers and Times Square - unless we work there, we never go there. So it was with a deep curiosity for this ginger from the Midwest that Justine agreed to meet Cory at the Toys R' Us in Times Square for their first date. It turns out he just wanted to watch her hula hoop, a feat which she failed miserably. I won't divulge how the rest of this date went, but let's just say more dates followed...",
-                four: "Spring turned to summer and Cory and Justine had been spending their days together more often than not. One warm summer evening, as they lay on the grass at the Hoboken waterfront watching Breakfast at Tiffany's, Cory turned to Justine and said something along the lines of, \"Since you've been leaving clothes at my apartment now, I might as well call you my girlfriend.\" And with those beautiful words they became solely each others.",
-                five: "Cory and Justine bore the grunt of busy schedules, commuting between Manhattan and Hoboken, for just shy of a year before they decided to move in together. They moved into the one building in lower Manhattan that would accept their 140 lb. dog, which as luck would have it, was also located right by the best movie theater and Shake Shack in New York.",
-                six: "Fast forward a year and a half, and in that very same apartment, Cory proposed to Justine, the sort of intimate and homemade proposal she had hoped for. With tears in her eyes and a croaking sound in her throat, she said yes…obviously…"
+                pre: "As the firstborn in her family, Mackenzie exhibited firstborn traits from the beginning. She had a hard time going to sleep on her own, and her parents would often stick her in her car seat on top of the washer and dryer to keep her calm while they got ready for the day. She was adorably serious, and very inquisitive. “Why is that man looking at me?” she asked once in the grocery store. Her favorite movie to watch at Nana and Poppie’s house was Fantasia (she loved the music), and to this day she still loves puzzles, crosswords and games.  ",
+                one: "As a little kid, Jonathan loved learning, loved trying new things and loved working with his hands. It didn’t take long for him to learn about tools and how to use them. Whenever he got a new toy, he’d play with it as it was intended and quickly move to testing its limits. If it rolled, how fast could it go? If it bounced, how high? If it came apart, what was inside and how did it work? He also loved being Mom and Dad’s little helper. These traits always amused them, and they began to understand that it was all part of the way he was wired. ",
+                two: "They go to high school together, but they don’t run in the same circles. He knows who she is and finds her mysterious. She, on the other hand, is completely oblivious to his existence. ",
+                three: "After Mackenzie’s junior year of college and Jonathan’s freshman year, they meet at Crossroads Bible Church’s college group. She’s attending Seattle Pacific University, and every Sunday she commutes across the lake to attend church and spend time with friends afterward. Finally, they’re running in the same circles, and she begins to realize that Jonathan’s quite a guy—charming and grounded, someone she considers the \“calm to her storm.\” He thinks she’s spontaneous and fun, and loves a partner for adventure. By the time the New Year rolls around, they’re hanging out one-on-one.",
+                four: "On March 31st, 2012, Jonathan takes Mackenzie out on their first date. They go to dinner, then spend hours in conversation as they walk around Green Lake.  Jonathan makes some unfortunate attempts to speak French and Mackenzie corrects his accent, completely charmed by his efforts. Their conversation continues over a warm cup of coffee and concludes with a trip to Kerry Park, where they gaze out at the Seattle night skyline. He asks her to be his girlfriend and they begin dating. Mackenzie’s weeks are a whirlwind of school, crew and struggling to get enough sleep, and Jonathan’s busy with school, too, but he drives to SPU from Bellevue every weekend so they can spend time together. After graduating, Mackenzie gets her first apartment in the U-District, just minutes away from Jonathan’s house. Hallelujah!",
+                five: "The week before Jonathan proposes, he reschedules their upcoming date. Mackenzie assumes that there’s no way he’s proposing that night. \“He wouldn’t reschedule,\” she thinks. \“He’s way too much of a planner to do that.\” It’s February 1st, 2014. He takes what she thought to be a strange route to Gasworks Park, and that’s when she starts to suspect something. They walk to the large hill to watch the sunset, and as they’re sitting on their blanket, Jonathan, earnest and teary-eyed, pops the question. Mackenzie’s in shock—she can’t believe it’s really happening. Naturally, she accepts. They celebrate with dinner at her favorite Italian restaurant, followed by dessert, her favorite meal, at Aqua El-Gaucho on the pier.",
+                six: "Come join us on Whidbey Island to celebrate! After you RSVP, you’ll receive an email with the full address and details."
             };
         },
         title: function() {
             return {
-                pre: "THEY MEET",
-                one: "THEY MEET",
-                two: "THEY MEET AGAIN",
-                three: "FIRST DATE",
-                four: "IT'S OFFICIAL",
-                five: "THEY SHACK UP",
-                six: "HE PROPOSES"
+                pre: "SHE IS BORN",
+                one: "HE IS BORN",
+                two: "THEY GROW UP",
+                three: "THEY MEET",
+                four: "THEY DATE",
+                five: "HE PROPOSES",
+                six: "THEY MARRY | WE CELEBRATE"
             };
         }
     };
