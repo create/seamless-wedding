30f9f78ca124c399a6fdf63456bc22e6b148338c
Jonathan Pyke <pyke.jonathan@gmail.com>
2014-05-24 23:22:51 -0700

    grr

--- modules/gallery/client/links/galleryLink.js
+++ modules/gallery/client/links/galleryLink.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
     var container = document.querySelector('#masongallery');
     var msnry = new Masonry( container, {
       // options
-      columnWidth: 200,
       itemSelector: '.item'
     });
    
