29ba028e8173d502d8fc1c04cd6a4c7dc67aba53
Jonathan Pyke <pyke.jonathan@gmail.com>
2014-05-25 07:51:18 -0700

    color

--- modules/story/client/links/storyContentLink.js
+++ modules/story/client/links/storyContentLink.js
@@ -47,7 +47,7 @@ module.exports = function (scope, element, attrs) {
         $image.parallax("50%", imageSpeed);
         $container.parallax("50%", contentSpeed);
         $poem.parallax("50%", poemSpeed, false, 0, "top");
-        $text.parallax("50%", textSpeed, false, 0, "bottom");
+        $text.parallax("80%", textSpeed, false, 0, "bottom");
     }
 
     scope.$on("PlayAnimation-" + attrs.id, function (event, data) {
