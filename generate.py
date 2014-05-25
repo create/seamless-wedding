7eec8caea9e30a084c21f22f96677532ae43aead
Jonathan Pyke <pyke.jonathan@gmail.com>
2014-05-25 00:26:39 -0700

    new photos

---
+++ generate.py
@@ -0,0 +1,5 @@
+import random
+
+for x in range(1, 60):
+	y = random.randint(1,3)
+	print "%s%d%s%d%s" % ('a.group(href="images/gallery/', x, '.jpg")\n    img.item(src="images/gallery/',x,'t.jpg")')
