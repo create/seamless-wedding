a1f0b1641f61a9890fbdf7a22b6ee08ed00634e9
Jeff Pyke <jeffpyke@uw.edu>
2014-05-24 19:59:33 -0700

    add password protected

--- modules/main/server/routes/routes.js
+++ modules/main/server/routes/routes.js
@@ -34,7 +34,7 @@ exports.getAllRsvps = function(req, res) {
 };
 
 exports.getAllRsvpInfo = function(req, res) {
-  if (req.params.password == "gethitched") {
+  if (req.body.password == "gethitched") {
     Rsvp.find({}, function(err, result) {
         if (!err) {
             return res.json({'rsvps': result});
