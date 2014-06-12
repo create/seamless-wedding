bd66b1da4ee89a47f2938f1ec61f756d1f709b52
Jeff <jeffpyke@uw.edu>
2014-06-12 12:16:48 -0700

    Changes to navbar

--- modules/common/client/common/common.js
+++ modules/common/client/common/common.js
@@ -11,12 +11,17 @@ var common = {
     resetBackground : function (element) {
         var $html = $(element.parents()[element.parents().length-1]);
         $html.find('.nav-item').find('a').css({color: '#333'});
-        $html.find('.nav-item').find('a').css({"text-shadow": 'none'});
+        $html.find('.nav-item').find('a').css({"text-shadow": '0 0 5px #fff;'});
+        $('body').css({background: 'url(/images/main/main-bg.jpg'});
+        $('.navigation').css({'background': "url('../images/main/bg-texture.png')",
+    'box-shadow': '0px 0px 5px #727272'});
     },
-    resetRegistryBackground : function (element) {
+    resetHomeBackground : function (element) {
         var $html = $(element.parents()[element.parents().length-1]);
         $html.find('.nav-item').find('a').css({color: '#FFFFFF'});
         $html.find('.nav-item').find('a').css({"text-shadow": '0 0 5px #111;'});
+        $('body').css({background: 'url(/images/home/bg-home.jpg'});
+        $('.navigation').css({'background': "transparent", 'box-shadow': 'none'});
     },
     resetScroll: function (element) {
         var $html = $(element.parents()[element.parents().length-1]);
