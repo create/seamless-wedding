56e3ed7c8d3c8db7f1b394e457bc1ecf477ac0d3
Jeff <jeffpyke@uw.edu>
2014-06-12 12:58:58 -0700

    Fix horrendus border radius

--- modules/common/client/common/common.js
+++ modules/common/client/common/common.js
@@ -14,7 +14,7 @@ var common = {
         $html.find('.nav-item').find('a').css({"text-shadow": '0 0 5px #fff;'});
         $('body').css({background: 'url(/images/main/main-bg.jpg'});
         var nav = $('.navigation');
-        if (nav.css("background") == "none") {
+        if (nav.css("background").indexOf("none") > -1) { // string contains
             $('.navigation').css({'background': "url('../images/main/bg-texture.png')",
     'box-shadow': '0px 0px 5px #727272'});
         }
