5cf33631452beae74091dcac1640b2acc9271aff
Jeff Pyke <jeffpyke@uw.edu>
2014-06-17 19:37:31 -0700

    Mobile style for all pages

--- modules/registry/client/links/registryLink.js
+++ modules/registry/client/links/registryLink.js
@@ -5,15 +5,4 @@ module.exports = function (scope, element, attrs) {
     common.resetBackground(element);
     common.resetFixed(element);
 
-    $.get("/get/rsvps/", function (data, status) {
-        for (var i = 0; i < data.rsvps.length; i++) {
-            if (data.rsvps[i].name != undefined && data.rsvps[i].message != undefined && data.rsvps[i].message !== "") {
-                var name = data.rsvps[i].name;
-                console.log("printing rsvp: " + name);
-                var message = data.rsvps[i].message;
-
-                $('#rsvps').prepend("<q>" + message + "</q><p class='attribution'>" + name + "</p>");
-            }
-        }
-    });
 };
